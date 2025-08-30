a2a6e1162006b2d8773a3d2f52cb12e42923abed
Santiago Lavigna Jara <santiagolavigna1@gmail.com>
2025-08-30 12:58:28 +0200

    removiendo precarga

--- index.js
+++ index.js
@@ -102,35 +102,6 @@
       pinFirstLevel: true
     });
 
-    function precargarTodosLosTiles(scene, geometry, callback) {
-    var stage = scene.stage();
-    var textureStore = stage.textureStore();
-
-    var pendientes = 0;
-
-    geometry.levelList().forEach(function(level) {
-      var size = level.size / level.tileSize;
-      ['l','r','u','d','f','b'].forEach(function(face) {
-        for (var y = 0; y < size; y++) {
-          for (var x = 0; x < size; x++) {
-            pendientes++;
-            var tile = { face: face, z: level.z, x: x, y: y };
-            textureStore.load(tile).then(function() {
-              pendientes--;
-              if (pendientes === 0 && callback) {
-                callback();
-              }
-            });
-          }
-        }
-      });
-    });
-  }
-  precargarTodosLosTiles(scene, geometry, function() {
-    console.log("Todos los tiles precargados");
-    scene.switchTo();
-  });
-
     // Create link hotspots.
     data.linkHotspots.forEach(function(hotspot) {
       var element = createLinkHotspotElement(hotspot);
